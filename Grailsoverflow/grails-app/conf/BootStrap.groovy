3451c49f07f22601a8667e68efe3fb72a6509180
Kévin Renella <kevin.renella@gmail.com>
2013-12-11 23:28:09 +0100

    Remove target folder

--- Grailsoverflow/grails-app/conf/BootStrap.groovy
+++ Grailsoverflow/grails-app/conf/BootStrap.groovy
@@ -5,6 +5,7 @@ class BootStrap {
 
     def init = { servletContext ->
         new Question(author:"Kevin", title:"How can I create  a Groovy Project ?").save()
+        new Question(author:"Florian", title:"Can you explain Kevin how to do :( ?").save()
     }
     def destroy = {
     }
